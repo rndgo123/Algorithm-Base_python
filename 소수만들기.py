079d5761d3af4e0c43525675334d81b2180bdd0a
SCH_26 <66462204+rndgo123@users.noreply.github.com>
2021-06-29 00:57:50 +0900

    Add files via upload

---
+++ 소수만들기.py
@@ -0,0 +1,19 @@
+def solution(nums):
+    n = len(nums)
+    answer = []
+    
+    def is_prime(x) :
+        if x < 2 :
+            return False
+        for i in range(2, x-1) :
+            if x % i == 0 :
+                return False
+        return True
+    
+    for i in range(n) :
+        for j in range(i+1, n) :
+            for m in range(j+1, n) :
+                x = nums[i] + nums[j] + nums[m]
+                if is_prime(x) == True :
+                    answer.append(x)
+    return len(answer)
