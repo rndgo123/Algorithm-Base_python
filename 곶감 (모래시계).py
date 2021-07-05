0fe324d22824f1fbe97825ea72e167193b47427d
SCH_27 <66462204+rndgo123@users.noreply.github.com>
2021-07-05 22:04:10 +0900

    Add files via upload

---
+++ 곶감 (모래시계).py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[102]:
+
+
+#나의 풀이 - 오답
+n = int(input())
+tree = [list]
+
+test = int(input())
+
+for _ in range(test) :
+    row, direction, num = map(int, input().split())
+    if direction == 0 :
+        left = tree[row-1][:num]
+        right = tree[row-1][num:]
+        right.extend(left)
+        tree[row-1] = right
+    else :
+        left = tree[row-1][:-num]
+        right = tree[row-1][-num:]
+        right.extend(left)
+        tree[row-1] = right
+        
+result = 0
+s = 0
+e = n - 1
+
+for i in range(n) :
+    for j in range(s, e+1) :
+        result += tree[i][j]
+    if i < n//2 :
+        s += 1
+        e -= 1
+    else :
+        s -= 1
+        e += 1
+        
+print(result)
+
+#풀이
+
+n = 5
+tree = [[10, 13, 10, 12, 15], 
+        [12, 39, 30, 23, 11], 
+        [11, 25, 50, 53, 15], 
+        [19, 27, 29, 37, 27], 
+        [19, 13, 30, 13, 19]]
+
+test = 3
+
+for i in range(test) :
+    row, direction, num = map(int, input().split())
+    if direction == 0 :
+        for _ in range(num) :
+            tree[row-1].append(tree[row-1].pop(0))
+    else :
+        for _ in range(num) :
+            tree[row-1].insert(0, tree[row-1].pop())
+result = 0
+start = 0
+end = n - 1
+
+for i in range(n) :
+    for j in range(start, end+1) :
+        result += tree[i][j]
+    if i < n//2 :
+        start += 1
+        end -= 1
+    else :
+        start -= 1
+        end += 1
+        
+print(result)
+
