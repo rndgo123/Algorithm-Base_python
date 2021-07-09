ac3b42857c7fb09d235005be50400cf0366fedb1
SCH_27 <66462204+rndgo123@users.noreply.github.com>
2021-07-10 01:54:47 +0900

    Update 키패드 누르기.py

--- 키패드 누르기.py
+++ 키패드 누르기.py
@@ -1,8 +1,8 @@
 def solution(numbers, hand):
-    l,r = '*', '#' #초기값 설정
+    l,r = '*', '#' 
     anwser = '' 
     
-    def dt(now, after) : #numbers in [2,5,8,0]일 때 거리구하는 함수
+    def dt(now, after) : 
         pad = {1:(0,0), 2:(0,1), 3:(0,2),
               4:(1,0), 5:(1,1), 6:(1,2),
               7:(2,0), 8:(2,1), 9:(2,2),
@@ -11,16 +11,16 @@ def solution(numbers, hand):
         now_x, now_y = pad[now]
         after_x, after_y = pad[after]
 
-        return abs(after_x-now_x)+abs(after_y-now_y) #점과 점 사이 거리
+        return abs(after_x-now_x)+abs(after_y-now_y) 
 
-    for i in numbers : #L
+    for i in numbers : 
         if i in [1,4,7] :
             anwser+='L'
             l = i
-        elif i in [3,6,9] : #R
+        elif i in [3,6,9] : 
             anwser+='R'
             r = i
-        elif i in [2,5,8,0] : #M
+        elif i in [2,5,8,0] : 
             l_dt = dt(l, i)
             r_dt = dt(r, i)
             if l_dt < r_dt :
