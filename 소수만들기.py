2be875e411b36be7798674f66c2ebe9762a9f246
SCH_27 <66462204+rndgo123@users.noreply.github.com>
2021-07-10 01:53:14 +0900

    Update 소수만들기.py

--- 소수만들기.py
+++ 소수만들기.py
@@ -1,8 +1,8 @@
 def solution(nums):
-    n = len(nums) #range 지정
+    n = len(nums)
     answer = []
     
-    def is_prime(x) : #소수 확인 함수
+    def is_prime(x) : 
         if x < 2 :
             return False
         for i in range(2, x-1) :
@@ -13,7 +13,7 @@ def solution(nums):
     for i in range(n) : 
         for j in range(i+1, n) :
             for m in range(j+1, n) :
-                x = nums[i] + nums[j] + nums[m] #범위를 한 개씩 올려 조합 3개의 숫자 합산 생성
+                x = nums[i] + nums[j] + nums[m] 
                 if is_prime(x) == True : 
                     answer.append(x)
     return len(answer)
